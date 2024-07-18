1f74f930f882c3f1a86cb00011526d1afb206937
M. Ilham Ramadhana <154799247+iphamm@users.noreply.github.com>
2024-07-19 00:16:02 +0700

    Add files via upload
    this is my first project

---
+++ to-do list/app.js
@@ -0,0 +1,11 @@
+document.getElementById('add-task').addEventListener('click', function() {
+    const taskInput = document.getElementById('new-task');
+    const taskText = taskInput.value;
+    if (taskText) {
+        const taskList = document.getElementById('task-list');
+        const newTask = document.createElement('li');
+        newTask.textContent = taskText;
+        taskList.appendChild(newTask);
+        taskInput.value = '';
+    }
+});
